23bcdf864fb40016dc0e4573078201d360af1c0a
EricaMarzi <erica.marzi.1993@gmail.com>
2024-02-26 18:25:38 +0100

    extends

--- Dog.php
+++ Dog.php
@@ -1,1 +1,19 @@
 <?php
+include __DIR__ . '/Product.php';
+
+ class DogProduct extends Product {
+    public $type;
+    public $icon;
+
+    public function __construct($name, $price, $code, $image, $genre, $type, $icon){
+        // parent::__construct($name, $price, $code, $image, $genre)
+        $this->name = $name;
+        $this->price = $price;
+        $this->code = $code;
+        $this->image = $image;
+        $this->genre = $genre;
+
+        $this->type = $type;
+        $this->icon = $icon;
+    }
+ }
--- Product.php
+++ Product.php
@@ -1,12 +1,15 @@
 <?php 
+//Food
+$prodotto1 = new Product ('Royal Canin Mini Adult', 5, 1, 'https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000', 'Food');
 
-$prodotto1 = new Product ('Royal Canin Mini Adult', 5, 1, 'https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000', 'Alimenti');
-$prodotto2 = new Product ('Almo Nature Holistic Maintenance', 5, 2, 'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', 'Alimenti');
-$prodotto3 = new Product ('Almo Nature Cat Daily Lattina', 5, 3, 'https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg', 'Alimenti');
-$prodotto4 = new Product ('Mangime Pesci Guppy in Fiocchi', 5, 4, 'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg', 'Alimenti');
+$prodotto2 = new Product ('Almo Nature Holistic Maintenance', 5, 2, 'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', 'Food');
+$prodotto3 = new Product ('Almo Nature Cat Daily Lattina', 5, 3, 'https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg', 'Food');
+$prodotto4 = new Product ('Mangime Pesci Guppy in Fiocchi', 5, 4, 'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg', 'Food');
 
-$prodotto5 = new Product ('Cartucce Filtranti per Filtro EasyCrystal', 8, 5, 'https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', 'Accessori');
-$prodotto6 = new Product ('Voliera Wilma in Legno', 8, 6, 'https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg', 'Accessori');
+//Accessories
+$prodotto5 = new Product ('Cartucce Filtranti per Filtro EasyCrystal', 8, 5, 'https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', 'Accessories');
+$prodotto6 = new Product ('Voliera Wilma in Legno', 8, 6, 'https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg', 'Accessories');
 
+//Toy
 $prodotto7 = new Product ('Kong Classic', 8, 7, 'https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg', 'Toy');
 $prodotto8 = new Product ('Topini di Peluche Trixie', 8, 8, 'https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg', 'Toy');
